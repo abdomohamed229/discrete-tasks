e39fed1cdc62af6a12a97206ca4e7c9f8b6aa070
abdomohamed229 <120930840+abdomohamed229@users.noreply.github.com>
2022-12-18 04:35:31 -0800

    Add files via upload

---
+++ project 1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace project_dr_samir_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the frist num...");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second num..");
+            int num2 = int.Parse(Console.ReadLine());
+            int i, j;
+
+            for (i = num; i <= num2; i++)
+            {
+                bool prime = true;
+
+                for (j = 2; j <= i - 1; j++)
+
+                {
+                    if (i % j == 0)
+                    {
+                        prime = false;
+                    }
+                  
+
+
+
+                }
+
+                if (prime == true)
+                {
+                    Console.WriteLine(i);
+
+                }
+
+            }
+        }
+    }
+}
---
+++ project 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace project_dr_sameer2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the frist number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            int num2 = int.Parse(Console.ReadLine());
+            int i;
+            for (i =num1; i <= num2; i++)
+            {
+                int result = FindFactor(i);
+                if (i == result)
+                {
+                    Console.WriteLine(i + " is the perfect number");
+                }
+
+            }
+
+        }
+        public static int FindFactor(int num2)
+        {
+            int sum = 0;
+            for (int i = 1 ; i < num2; i++)
+            {
+                if (num2 % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            return sum;
+        }
+        
+       
+
+    }
+}    
